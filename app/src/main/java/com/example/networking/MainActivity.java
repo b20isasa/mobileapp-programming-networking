15d26ed91fdf01b62e925b8d7ff220b081f2c7f7
b20isasa <b20isasa@student.his.se>
2023-04-27 10:04:19 +0200

    dek. alla variabler och arraylista.

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -4,10 +4,16 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
-
+    private ArrayList<Mountains> mountainsList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private Myadapter adapter;
     private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
     private final String JSON_FILE = "mountains.json";
 
@@ -15,7 +21,13 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        recyclerView = findViewById(R.id.recyclerView);
+        adapter = new Myadapter(mountainsList);
+        recyclerView.setAdapter(adapter);
+        adapter = new Myadapter(mountainsList);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager((new LinearLayoutManager(this)));
         new JsonFile(this, this).execute(JSON_FILE);
     }
 
