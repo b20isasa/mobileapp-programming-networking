ac38f7f14aff83f037f19cc446381fdd244d5620
b20isasa <b20isasa@student.his.se>
2023-05-04 10:02:29 +0200

    E104.

--- app/src/main/java/com/example/networking/Myadapter.java
+++ app/src/main/java/com/example/networking/Myadapter.java
@@ -1,33 +1,4 @@
 package com.example.networking;
 
-import android.view.ViewGroup;
-
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Myadapter extends RecyclerView.Adapter {
-private List<String> data;
-
-    public Myadapter(ArrayList<Mountains> mountainsList) {
-
-    }
-
-    @NonNull
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
+public class MyAdapter {
 }
