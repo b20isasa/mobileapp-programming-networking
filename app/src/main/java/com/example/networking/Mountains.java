3ce88d70331385d6a0c6e1e748345f381457d2a9
b20isasa <b20isasa@student.his.se>
2023-05-14 14:32:43 +0200

    github test

--- app/src/main/java/com/example/networking/Mountains.java
+++ app/src/main/java/com/example/networking/Mountains.java
@@ -1,14 +1,28 @@
 package com.example.networking;
 
 public class Mountains {
+    private String name;
+    private int size;
 
-/*
-        private String ID;
-        private String name;
-        @SerializedName("cost")
-        private int feet;
-        @SerializedName("size")
-        private int meter;*/
+    public Mountains(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
     }
 
